bdc84eb8e2d6ac0531bb6d29647956ec3d842f79
archcorsair <archcorsair@gmail.com>
2016-10-06 20:43:29 -0700

    Complete advanced content up to advancedChaining.js

--- exercises/advanced/advancedChaining.js
+++ exercises/advanced/advancedChaining.js
@@ -2,13 +2,13 @@
  * Your task is to write a function that uses a deep learning
  * algorithm to determine the common set of tags between
  * multiple github profile pictures
- * 
+ *
  * Given an array of github handles, searchCommonTagsFromGitHubProfiles should:
  *   1) get the public profile associated with each handle
  *   2) extract the avatar_url of each profile
  *   4) get the set of tags for each avatar_url (requires authentication)
  *   5) find the intersection of the tags
- * 
+ *
  * Much of the heavy lifting has been done already in `lib/advancedChainingHelpers`,
  * you just have to wire everything up together! Once you pass this one, you'll
  * be a promise chaining master! Have fun!
@@ -16,14 +16,27 @@
 
 var Promise = require('bluebird');
 var lib = require('../../lib/advancedChainingLib.js');
+var request = require('request');
 
 // We're using Clarifai's API to recognize different an image into a list of tags
 // Visit the following url to sign up for a free account
 //     https://developer.clarifai.com/accounts/login/?next=/applications/
 // Then, create a new Application and pass your Client Id and Client Secret into the method below
-lib.setImageTaggerCredentials('YOUR_CLIENT_ID', 'YOUR_CLIENT_SECRET')
+lib.setImageTaggerCredentials('1Htaeritxg0MCrz871VtuKU76dHGxU7GggV4uwzN', 'jRw0WLjs82XPgzhGsQughxcc34iMkBwcm0WYI1rV');
+var accessToken = '3UAGQg6hWndhfzjFu1xPaRBItPivz8';
 
 var searchCommonTagsFromGitHubProfiles = function(githubHandles) {
+  return Promise.all(githubHandles.map(handle => (lib.getGitHubProfile(handle))))
+    .then((profiles) => {
+      console.log(profiles);
+      return profiles.map(profile => (profile.avatarUrl));
+    })
+    .then(
+      function(bagels) {
+        console.log('What are teh baglez?', bagels);
+        return lib.authenticateImageTagger().then(lib.tagImage.bind(null, bagels));
+      }
+    ).then(lib.getIntersection);
 };
 
 // Export these functions so we can unit test them
--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+var Promise = require('bluebird');
 /**
  * Using Promise.all, write a function, combineFirstLineOfManyFiles, that:
  *    1. Reads each file at the path in the `filePaths` array
@@ -7,12 +9,42 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
+var readFirstLineAsync = function readFirstLineAsync(readFilePath) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(readFilePath, function(error, data) {
+      if (error) {
+        reject(error);
+      }
+      data = data.toString();
+      data = data.slice(0, data.indexOf('\n'));
+      resolve(data);
+    });
+  });
+};
 
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
-};
 
+  var writeArrayToFileAsync = function writeArrayToFileAsync(array) {
+    return new Promise(function(resolve, reject) {
+      var text = array.join('\n');
+      fs.writeFile(writePath, text, function(e) {
+        if (e) {
+          reject(e);
+        } else {
+          resolve();
+        }
+      });
+    });
+  };
+
+
+  return Promise.all(filePaths.map(filePath => readFirstLineAsync(filePath))).then(writeArrayToFileAsync).catch(console.log.bind(console));
+};
+//
+// var filePaths = ['/Users/student/Documents/2016-09-promises/testdata/file1.txt', '/Users/student/Documents/2016-09-promises/testdata/file2.txt', '/Users/student/Documents/2016-09-promises/testdata/file3.txt'];
+// var writePath = '/Users/student/Documents/2016-09-promises/testdata/results.txt';
+// combineFirstLineOfManyFiles(filePaths, writePath);
 // Export these functions so we can unit test them
 module.exports = {
   combineFirstLineOfManyFiles: combineFirstLineOfManyFiles
